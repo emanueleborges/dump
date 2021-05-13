7eb4bc926843dce98454b3d8097d1f111d36ef3a
emanueleborges <83500558+emanueleborges@users.noreply.github.com>
2021-05-13 09:13:33 -0400

    'commit heroku node js monbodb

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const bodyParser      = require('body-parser')
 const express         = require('express')
 const expressLayouts  = require('express-ejs-layouts')
-//const session = require('express-session')
+//const session       = require('express-session')
 const path            = require('path')
 const exceljs         = require('exceljs')
 const dotenv          = require('dotenv');
 
-
-const app = express()
-const connection = require('./app/config/connection')
+const app             = express()
+const connection      = require('./app/config/connection')
 connection.connectToServer(console.log)
 
 app.set('view engine', 'ejs')
@@ -16,7 +15,6 @@ app.use(expressLayouts)
 app.set('views', path.join(__dirname, 'app', 'views'))
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }));
-
 
 app.use('/', require('./app/config/routes'))
 
